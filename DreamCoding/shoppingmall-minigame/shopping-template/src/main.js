e5c9c169de5fb995c9f617be9157f4d13e36b81f
Jang-oi <jjh53241@gmail.com>
2021-03-05 11:08:53 +0900

    드림코딩 코드 분석?

--- DreamCoding/shoppingmall-minigame/shopping-template/src/main.js
+++ DreamCoding/shoppingmall-minigame/shopping-template/src/main.js
@@ -6,15 +6,15 @@ function loadItems() {
         .then(json => json.items);
     // fetch 쉬움 공부!
 }
-
-//
+// items 를 가공해서 브라우저에 뿌려줌.
 function displayItems(items) {
+    // items 안에 있는 첫번째 요소를 container 에 담는다.
     const container = document.querySelector('.items');
-    const html = items.map(item => createHTMLString(item)).join('');
-    console.log(html);
+    // container 에 받아온 items 를 map 형식으로 만들어서 뿌림?
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// HTML형식으로 item 들에 대한 값들을 넣어서 리턴함
 function createHTMLString(item) {
     return `
     <li class="item">
@@ -25,11 +25,16 @@ function createHTMLString(item) {
 }
 
 // main
+// 아이템을 불러오는 함수 실행
 loadItems()
+    // items 가 담겨 오면
+    // items 가 보여지는 함수를 실행하고
+    // 이벤트가 실행 되서 원하는 items 를 보여지는 함수를 싱행.
     .then(items => {
-        console.log(items);
+        // console.log(items);
         displayItems(items);
         // setEventListeners(items)
+    // reject 할 경우
     })
     .catch(console.log);
 
