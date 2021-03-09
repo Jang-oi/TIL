a7bb57b032f4dfd5f45c851189a64b013717cd05
Jang-oi <jjh53241@gmail.com>
2021-03-09 22:51:12 +0900

    [add] 바닐라 JS 로 크롬 앱 만들기 2일차

--- NomadCoder/바닐라 JS로 크롬 앱 만들기/index.js
+++ NomadCoder/바닐라 JS로 크롬 앱 만들기/index.js
@@ -1,1 +1,11 @@
 console.log('Im Working. Im JS. Im Beautiful. Im worth it')
+
+// const a = [1, [1,2]];
+
+const a = [1, {name: 'aa'}];
+
+console.log(a);
+
+const b = {name: 'aa', age: '33', test: [1, 2, 3]};
+
+console.log(b.test[0]);
